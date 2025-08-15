10f642f3e419f7e229e650435ff0bcbb5902b09c
alina-sozoeva <sozoeva.alina@gmail.com>
2025-08-15 17:50:30 +0600

    fixed

--- src/pages/registerPage/RegisterPage.jsx
+++ src/pages/registerPage/RegisterPage.jsx
@@ -1,37 +1,15 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, Input, TextField, Typography } from "@mui/material";
 import styles from "./RegisterPage.module.scss";
 import clsx from "clsx";
+import { Controller, useForm } from "react-hook-form";
 
 export const RegisterPage = () => {
-  function Item(props) {
-    const { sx, ...other } = props;
-    return (
-      <Box
-        sx={[
-          (theme) => ({
-            p: 1,
-            m: 1,
-            bgcolor: "grey.100",
-            color: "grey.800",
-            border: "1px solid",
-            borderColor: "grey.300",
-            borderRadius: 2,
-            fontSize: "0.875rem",
-            fontWeight: "700",
-            ...theme.applyStyles("dark", {
-              bgcolor: "#101010",
-              color: "grey.300",
-              borderColor: "grey.800",
-            }),
-          }),
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-        {...other}
-      />
-    );
-  }
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <main>
+    <main className={clsx(styles.main)}>
       <h2 className={clsx(styles.title)}>Регистрация</h2>
       <Box
         sx={{
@@ -41,13 +19,50 @@ export const RegisterPage = () => {
           m: 1,
           bgcolor: "background.paper",
           borderRadius: 1,
+          gap: "10px",
         }}
       >
-        <Item>Клиент</Item>
-        <Item>Профиль</Item>
-        <Item>Забор</Item>
-        <Item>Анализы</Item>
+        <Button variant="contained">Клиент</Button>
+        <Button variant="contained">Профиль</Button>
+        <Button variant="contained">Забор</Button>
+        <Button variant="contained">Анализы</Button>
       </Box>
+      <form onSubmit={handleSubmit(onSubmit)} className={clsx(styles.form)}>
+        <Controller
+          name="inn"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+              }}
+            >
+              <Typography variant="subtitle1">ИНН</Typography>
+              <TextField label="Введите ИНН" />
+            </Box>
+          )}
+        />
+        <Controller
+          name="username"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+              }}
+            >
+              <Typography variant="subtitle1">ФИО пациента</Typography>
+              <TextField label="Введите ФИО пациента" />
+            </Box>
+          )}
+        />
+      </form>
     </main>
   );
 };
